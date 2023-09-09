66a1673d72ee88866499330fca88c2ce07719b29
Renan Acuin <racuin2593@gmail.com>
2023-09-09 12:22:59 +0800

    Update Navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,10 +3,10 @@ import Search from "./Search";
 
 export default function NavBar() {
   return (
-    <header className="bg-black sticky top-0 z-10">
-      <nav className="flex flex-col gap-4 sm:flex-row sm:justify-between items-center p-4 font-bold max-w-6xl mx-auto text-white">
+    <header className="bg-sky-500 sticky top-0 z-10">
+      <nav className="flex flex-col gap-4 sm:flex-row sm:justify-between items-center p-4 font-bold max-w-6xl mx-auto text-black font-serif hover:font-sans">
         <h1 className="text-2xl sm:text-3xl text-center whitespace-nowrap">
-          <Link href="/">Next.js Image Gallery</Link>
+          <Link href="/">Ren Image Gallery</Link>
         </h1>
         <Search />
       </nav>
